1d10ff0e4a5a4d19f790aadb3567b5c34eb2e720
melevanet <157515892+melevanet@users.noreply.github.com>
2024-03-23 11:35:36 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randonindexExcusas = length => {
+    return Math.floor(Math.random() * length);
+  };
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomExcuse = `${who[randonindexExcusas(who.length)] +
+    " " +
+    action[randonindexExcusas(action.length)] +
+    " " +
+    what[randonindexExcusas(what.length)]}`;
+  " " + when[randonindexExcusas(action.length)];
+
+  document.getElementById("excuse").innerHTML = randomExcuse;
+
+  console.log("hello ringo from the console");
 };
